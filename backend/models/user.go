22e4fe2c951165c2f1e96ee3c60d6d3ca2692ae8
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-02-22 21:24:25 +0900

    qor framework seems to be obsolete.... :\

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,14 +1,13 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
-	"github.com/qor/auth"
+  "github.com/jinzhu/gorm"
 )
 
 // User model.
 type User struct {
 	gorm.Model
-	Username  string  `json:"username"`
+	Username  string  `json:"username" gorm:"vchar(256);unique_index"`
 	Email     string  `json:"email"`
 	Password  string  `json:"password"`
 	FirstName *string `json:"firstName"`
